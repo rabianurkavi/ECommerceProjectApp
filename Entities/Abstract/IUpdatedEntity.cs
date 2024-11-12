40b52ca05d123e78437d2e9ae0667c68919fd43d
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-13 02:25:27 +0300

    IUpdatedEntity oluşturuldu

---
+++ Entities/Abstract/IUpdatedEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Abstract
+{
+    public interface IUpdatedEntity
+    {
+        int? UpdatedId { get; set; }
+        DateTime? UpdatedDate { get; set; }
+    }
+}
