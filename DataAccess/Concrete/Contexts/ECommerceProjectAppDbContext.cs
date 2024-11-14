0c9b63fe038fad00ed2c035b3889eb9b97b17dd1
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-14 19:58:35 +0300

    Created of Context

---
+++ DataAccess/Concrete/Contexts/ECommerceProjectAppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.Contexts
+{
+    public class ECommerceProjectAppDbContext:DbContext
+    {
+        public ECommerceProjectAppDbContext(DbContextOptions<ECommerceProjectAppDbContext> options):base(options)
+        {
+            
+        }
+        public ECommerceProjectAppDbContext()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=RABIA\\SQLEXPRESS;Database=ECommerceProjectDB;Trusted_Connection=True;");
+        }
+    }
+}
