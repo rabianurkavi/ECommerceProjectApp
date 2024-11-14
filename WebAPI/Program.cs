80843c279fbafde11fb3906ced122510eccb8e3c
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-14 20:01:41 +0300

    DbContext added to service

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,4 +1,7 @@
+using DataAccess.Concrete.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ECommerceProjectContext");
 
 // Add services to the container.
 
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ECommerceProjectAppDbContext>();
 
 var app = builder.Build();
 
