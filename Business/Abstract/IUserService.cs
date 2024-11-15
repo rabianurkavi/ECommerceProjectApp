979899ba6640b2727c890cb083777dabfbd658b3
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-15 12:59:02 +0300

    Created of IUserService

---
+++ Business/Abstract/IUserService.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using Entities.Dtos.UserDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<UserDetailDto>> GetListAsync();
+        Task<UserDto> GetByIdAsync(int id);
+        Task<UserDto> AddAsync(UserAddDto entity);
+        Task<UserUpdateDto> UpdateAsync(UserUpdateDto user);
+        Task<bool> DeleteAsync(int id);
+    }
+}
