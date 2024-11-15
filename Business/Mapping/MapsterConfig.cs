a5ee3db5f6430d7761918b13aec2224e87c50f76
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-15 12:59:48 +0300

    Created of MapsterConfig

---
+++ Business/Mapping/MapsterConfig.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using Entities.Dtos.UserDtos;
+using Mapster;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Mapping
+{
+    public static class MapsterConfig
+    {
+        public static void RegisterMappings()
+        {
+            TypeAdapterConfig<User, UserAddDto>.NewConfig();
+            TypeAdapterConfig<User, UserDto>.NewConfig();
+            TypeAdapterConfig<User, UserUpdateDto>.NewConfig();
+            TypeAdapterConfig<User, UserDetailDto>.NewConfig();
+        }
+    }
+}
