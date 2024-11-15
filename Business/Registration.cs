ce68561a7b4bd0a5f304e03cc27a26cba06fc732
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-15 16:45:28 +0300

    Added Scoped with Business layer

---
+++ Business/Registration.cs
@@ -0,0 +1,20 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public static class Registration
+    {
+        public static void AddBusiness(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddScoped<IUserService, UserService>();
+        }
+    }
+}
