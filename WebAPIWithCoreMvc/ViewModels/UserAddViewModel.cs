bd3d23220621cc4f7501bf44b84d252ae558274c
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-27 00:40:47 +0300

    changes property GenderId

--- WebAPIWithCoreMvc/ViewModels/UserAddViewModel.cs
+++ WebAPIWithCoreMvc/ViewModels/UserAddViewModel.cs
@@ -6,7 +6,7 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Password { get; set; }
-        public int GenderID { get; set; }
+        public int GenderId { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string Email { get; set; }
         public string Adress { get; set; }
