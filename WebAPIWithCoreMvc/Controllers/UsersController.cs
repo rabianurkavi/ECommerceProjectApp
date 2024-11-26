cc59b838c5992e5ac13ec68870112aaedf057a75
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-26 23:40:42 +0300

    Created UserController for mvc

---
+++ WebAPIWithCoreMvc/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Dtos.UserDtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIWithCoreMvc.Controllers
+{
+    public class UsersController : Controller
+    {
+        #region Defines
+        private readonly HttpClient _httpClient;
+        private string url = "http://localhost:5065/api/";
+        #endregion
+        #region Constructor
+        public UsersController(HttpClient httpClient)
+        {
+            _httpClient=httpClient;
+        }
+        #endregion
+        public async Task<IActionResult> Index()
+        {
+            var users = await _httpClient.GetFromJsonAsync<List<UserDetailDto>>(url+"User/GetList");
+            return View(users);
+        }
+    }
+}
