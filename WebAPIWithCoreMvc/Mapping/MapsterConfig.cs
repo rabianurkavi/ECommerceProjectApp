7fa9c18f25262597e493cb9c654a80ce862004bf
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-27 00:39:54 +0300

    created mapsterconfig for MVC

---
+++ WebAPIWithCoreMvc/Mapping/MapsterConfig.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using Entities.Dtos.UserDtos;
+using Mapster;
+using Microsoft.AspNetCore.Routing.Constraints;
+using WebAPIWithCoreMvc.ViewModels;
+
+namespace WebAPIWithCoreMvc.Mapping
+{
+    public static class MapsterConfig
+    {
+        public static void RegisterMappings()
+        {
+            TypeAdapterConfig<UserAddViewModel, UserAddDto>.NewConfig()
+             .Map(dest => dest.Gender, src => src.GenderId == 1);
+        }
+    }
+}
