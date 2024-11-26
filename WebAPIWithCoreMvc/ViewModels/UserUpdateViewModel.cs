072b5f77260ef956bff376d4c1c210f0c8b59fcc
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-27 02:36:46 +0300

    created

---
+++ WebAPIWithCoreMvc/ViewModels/UserUpdateViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace WebAPIWithCoreMvc.ViewModels
+{
+    public class UserUpdateViewModel
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public int GenderId { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string Adress { get; set; }
+    }
+}
