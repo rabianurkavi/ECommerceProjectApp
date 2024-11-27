81716c6ae98736bd960fd12d4aaaf337ec164478
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-28 01:32:41 +0300

    Added Authenticate Operations

--- Business/Concrete/UserService.cs
+++ Business/Concrete/UserService.cs
@@ -1,11 +1,16 @@
 ﻿using Business.Abstract;
+using Core.Helpers.JWT;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.Dtos.UserDtos;
 using Mapster;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,15 +19,44 @@ namespace Business.Concrete
     public class UserService : IUserService
     {
         private readonly IUserDal _userDal;
-        public UserService(IUserDal userDal)
+        AppSettings _appSettings;
+        public UserService(IUserDal userDal, IOptions<AppSettings> appSettings)
         {
-            _userDal = userDal; 
+            _userDal = userDal;
+            _appSettings = appSettings.Value;
         }
         public async Task<UserDto> AddAsync(UserAddDto entity)
         {
             var user = entity.Adapt<User>();
             var addedUser = await _userDal.AddAsync(user);
             return addedUser.Adapt<UserDto>();
+        }
+
+        public async Task<AccessToken> Authenticate(UserForLoginDto userForLoginDto)
+        {
+            var user = await _userDal.GetAsync(x => x.UserName == userForLoginDto.UserName && x.Password == userForLoginDto.Password);
+
+            var tokenHandler= new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.SecurityKey);
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name,user.ID.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token= tokenHandler.CreateToken(tokenDescriptor);
+            AccessToken accessToken = new AccessToken()
+            {
+                Token = tokenHandler.WriteToken(token),
+                UserName=user.UserName,
+                Expiration= (DateTime)tokenDescriptor.Expires,
+                UserID=user.ID,
+            };
+            return await Task.Run(() => accessToken);
         }
 
         public async Task<bool> DeleteAsync(int id)
