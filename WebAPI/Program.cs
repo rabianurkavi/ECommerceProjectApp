070d65c47e178d28420386b5deecddae1122a588
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-28 01:34:56 +0300

    added JWT Configurations

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -2,9 +2,14 @@ using Business;
 using Business.Abstract;
 using Business.Concrete;
 using Business.Mapping;
+using Core.Helpers.JWT;
 using DataAccess.Abstract;
 using DataAccess.Concrete.Contexts;
 using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,8 +18,58 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IUserService,UserService>();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Bearer þemasýný kullanan JWT Yetkilendirme baþladý \r\n\r\n 'Bearer' [boþluk] girin ve tokený yazýnýz. "
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference=new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
+
+#region JWT
+builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
+// Authentication ve JWT ayarlarý
+var appSettings = builder.Configuration.GetSection("AppSettings").Get<AppSettings>();
+var key = Encoding.ASCII.GetBytes(appSettings.SecurityKey);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false, // Eðer issuer kontrolü yapmak isterseniz true yapýn
+        ValidateAudience = false, // Audience kontrolü yoksa false býrakýn
+        ValidateLifetime = true, // Token süresini doðrular
+        ValidateIssuerSigningKey = true, // Security key doðrulamasý
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+});
+#endregion
+builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IUserDal, EfUserDal>();
 
 builder.Services.AddDbContext<ECommerceProjectAppDbContext>();
@@ -32,8 +87,8 @@ if (app.Environment.IsDevelopment())
     });
 }
 
-
-    app.UseAuthorization();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
