20ce157bee8351d3f148ed83bade66a2a980388a
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-27 23:24:26 +0300

    Created core layer and to this layer move codes of entity layer dataaccess layer

---
+++ Core/Entity/Abstract/IUpdatedEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entity.Abstract
+{
+    public interface IUpdatedEntity
+    {
+        int? UpdatedId { get; set; }
+        DateTime? UpdatedDate { get; set; }
+    }
+}
---
+++ Core/Entity/Concrete/AuditableEntity.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entity.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entity.Concrete
+{
+    public class AuditableEntity : BaseEntity, ICreatedEntity, IUpdatedEntity
+    {
+        public int? UpdatedId { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public int CreatedId { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
